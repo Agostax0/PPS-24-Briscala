e1be9a2b0538d659d2de3bd80eeab30200a34328
Riccardo Alni <riccardo.alni@studio.unibo.it>
2025-06-24 13:04:24 +0200

    [feat] can add players via dsl

--- src/main/scala/dsl/GameBuilder.scala
+++ src/main/scala/dsl/GameBuilder.scala
@@ -1,18 +1,21 @@
 package dsl
 
 import dsl.types.PlayerCount
-import engine.model.PlayerModel
+import engine.model.{EngineModel, PlayerModel}
 
 sealed trait GameBuilder:
   val gameName: String
   def addPlayer(name: String): GameBuilder
   def setPlayers(n: Int): GameBuilder
+  def build(): EngineModel
+
 object GameBuilder:
   def apply(gameName: String): GameBuilder = GameBuilderImpl(gameName)
 
   private class GameBuilderImpl(val gameName: String) extends GameBuilder:
     private var players: List[PlayerModel] = List.empty
     private var playerCount: PlayerCount = _
+
     override def addPlayer(name: String): GameBuilder =
       players = players :+ PlayerModel(name)
       this
@@ -20,3 +23,11 @@ object GameBuilder:
     override def setPlayers(n: Int): GameBuilder =
       playerCount = PlayerCount(n)
       this
+
+    override def build(): EngineModel =
+      if !playerCount.equals(PlayerCount(players.size)) then
+        throw new IllegalArgumentException("Incorrect number of players joined")
+
+      val game = EngineModel()
+      game.addPlayers(players)
+      game

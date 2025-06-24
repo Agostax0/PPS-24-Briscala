9dbb81d5e363e8343abccba55d0246701d7756fb
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2025-06-24 17:43:56 +0200

    [feat] "setup players" task completed

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,8 +1,19 @@
-package it.unibo
+import dsl.{GameBuilder, GameDSL}
+import dsl.GameDSL.*
+import dsl.syntax.SyntacticSugar.*
+import engine.controller.EngineController
 
-
+import scala.language.implicitConversions
+import scala.language.postfixOps
 @main
-def main(): Unit = {
-  println("Hello world!")
-}
+def main(): Unit =
 
+  game is "Briscala"
+  game has 2 players
+
+  game has player called "Alice"
+  game has player called "Bob"
+
+  EngineController(game.build()).start()
+
+end main

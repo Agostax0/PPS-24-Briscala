141c7f26527d4b7df2a0cf8e55ed5e60b986d6b4
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2025-06-24 17:52:37 +0200

    [test] fix EngineModel input

--- src/test/scala/engine/model/EngineModelTest.scala
+++ src/test/scala/engine/model/EngineModelTest.scala
@@ -6,11 +6,11 @@ import org.scalatest.matchers.should
 class EngineModelTest extends AnyFlatSpec with should.Matchers :
 
   "An EngineModel" should "be correctly instantiated" in:
-    val engine = EngineModel()
+    val engine = EngineModel("")
     engine.players shouldBe empty
 
   it should "allow adding players" in:
-    val engine = EngineModel()
+    val engine = EngineModel("")
     val player1 = PlayerModel("Alice")
     val player2 = PlayerModel("Bob")
 

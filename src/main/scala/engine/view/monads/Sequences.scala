1d60999f2b51e83041c4581f05ceadc1054c599c
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2025-06-24 17:41:07 +0200

    [feat] team added util classes

---
+++ src/main/scala/engine/view/monads/Sequences.scala
@@ -0,0 +1,38 @@
+package engine.view.monads
+
+object Sequences:
+
+  enum Sequence[E]:
+    case Cons(head: E, tail: Sequence[E])
+    case Nil()
+
+  object Sequence:
+    def cons[E](head: E, tail: Sequence[E]): Sequence[E] = Cons(head, tail)
+    def nil[E](): Sequence[E] = Nil()
+
+    extension (l: Sequence[Int])
+      def sum: Int = l match
+        case Cons(h, t) => h + t.sum
+        case _          => 0
+
+    extension [A](l: Sequence[A])
+
+      def map[B](mapper: A => B): Sequence[B] = l match
+        case Cons(h, t) => Cons(mapper(h), t.map(mapper))
+        case Nil()      => Nil()
+
+      def filter(pred: A => Boolean): Sequence[A] = l match
+        case Cons(h, t) if pred(h) => Cons(h, t.filter(pred))
+        case Cons(_, t)            => t.filter(pred)
+        case Nil()                 => Nil()
+
+      def flatMap[B](mapper: A => Sequence[B]): Sequence[B] = l match
+        case Cons(h, t) => mapper(h).concat(t.flatMap(mapper))
+        case Nil()      => Nil()
+
+      def concat(other: Sequence[A]): Sequence[A] = l match
+        case Cons(h, t) => Cons(h, t.concat(other))
+        case Nil()      => other
+
+    def of[A](n: Int, a: A): Sequence[A] =
+      if (n == 0) then Nil[A]() else Cons(a, of(n - 1, a))

12684e3511dccbe0815dbb914b34e7a6f63a97be
Riccardo Alni <riccardo.alni@studio.unibo.it>
2025-06-25 11:45:47 +0200

    [feat] adding deck in EngineModel

--- src/main/scala/dsl/types/GameElements.scala
+++ src/main/scala/dsl/types/GameElements.scala
@@ -7,3 +7,25 @@ object PlayerCount:
   def apply(count: Int): PlayerCount =
     require(count >= 2 && count <= 4, "player count should be between 2 and 4")
     count
+
+opaque type HandSize = Int
+object HandSize:
+  def apply(size: Int): HandSize =
+    require(size >= 3 && size <= 10, "hand size should be between 3 and 10")
+    size
+    
+opaque type Suits = List[String]
+object Suits:
+  def apply(suits: List[String]): Suits =
+    require(suits.size == 4, "only 4 suits are allowed")
+    require(suits.distinct.size == 4, "suits must be distinct")
+    suits
+    
+  extension (suits: Suits)
+    def size: Int = suits.size
+    def distinct: List[String] = suits.distinct
+    def map[B](f: String => B): List[B] = suits.map(f)
+    def flatMap[B](f: String => List[B]): List[B] = suits.flatMap(f)
+    def foreach(f: String => Unit): Unit = suits.foreach(f)
+    
+    
--- src/main/scala/engine/model/EngineModel.scala
+++ src/main/scala/engine/model/EngineModel.scala
@@ -1,7 +1,17 @@
 package engine.model
 
-class EngineModel(val gameName: String):
-  var players: List[PlayerModel] = List.empty
+import dsl.types.Suits
 
+trait DeckManagement:
+  var deck: DeckModel = DeckModel()
+  
+  def createDeck(suits: Suits, ranks: List[String]): Unit =
+    for rank <- ranks 
+      suit <- suits do
+        deck.addCard(CardModel(rank, ranks.indexOf(rank), suit))
+
+class EngineModel(val gameName: String) extends DeckManagement:
+  var players: List[PlayerModel] = List.empty
+  
   def addPlayers(players: List[PlayerModel]): Unit =
     this.players = players

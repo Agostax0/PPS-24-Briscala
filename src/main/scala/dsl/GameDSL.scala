7504f8784262aca9946108cda490bef81f119eb2
Agosta Alessandro <agosta.alessandro@studio.unibo.it>
2025-06-25 10:42:43 +0200

    [docs] added scaladoc

--- src/main/scala/dsl/GameDSL.scala
+++ src/main/scala/dsl/GameDSL.scala
@@ -4,7 +4,7 @@ import dsl.syntax.SyntacticSugar.PlayerSyntacticSugar
 import dsl.syntax.SyntacticSugarBuilder.{PlayerBuilder, PlayerCountBuilder}
 
 object GameDSL:
-  private var builder: GameBuilder = GameBuilder("")
+  private var builder: GameBuilder = _
 
   def apply(gameBuilder: GameBuilder): Unit = builder = gameBuilder
   implicit def game: GameBuilder = builder
@@ -13,6 +13,11 @@ object GameDSL:
 
     /** Creates a new game with the given name. To be used as first rule: it
       * erases any previous configuration.
+     *
+      * To be used like this:
+     * {{{
+     *  game is "Game Name"
+     * }}}
       * @param name
       *   Game name
       * @return
@@ -22,8 +27,28 @@ object GameDSL:
       builder = GameBuilder(name)
       builder
 
+    /**
+     * Sets the game to have a specific number of players.
+     *
+     * To be used like this:
+     * {{{
+     *   game has 2 players
+     * }}}
+     * @param playerCount the number of players expected to be added
+     * @return The builder for the game
+     */
     infix def has(playerCount: Int): PlayerCountBuilder =
       PlayerCountBuilder(gameBuilder, playerCount)
 
+    /**
+     * Adds a player to the game.
+     *
+     * To be used like this:
+     * {{{
+     *   game has player called "Alice"
+     * }}}
+     * @param playerSyntax
+     * @return The builder for the game
+     */
     infix def has(playerSyntax: PlayerSyntacticSugar): PlayerBuilder =
       PlayerBuilder(gameBuilder)

a6e79c70ebb5577705787088ab4adf91c87cd99b
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2025-07-09 11:21:18 +0200

    [docs] added scaladoc

--- src/main/scala/dsl/syntax/SyntacticSugar.scala
+++ src/main/scala/dsl/syntax/SyntacticSugar.scala
@@ -3,19 +3,94 @@ package dsl.syntax
 trait SyntacticSugar
 
 object SyntacticSugar:
+  /** Syntactic sugar to make the DSL more natural language-like
+    *
+    * This is used in the syntax: {{{game has player called "Alice"}}} And
+    * {{{game gives 3 cards to every player}}}
+    */
   implicit val player: PlayerSyntacticSugar = PlayerSyntacticSugar
+
+  /** Syntactic sugar to make the DSL more natural language-like
+    *
+    * This is used in the syntax: {{{game has randomBot called "Josh"}}}
+    */
   implicit val randomBot: EntitySyntacticSugar = RandomBotSyntacticSugar
+
+  /** Syntactic sugar to make the DSL more natural language-like
+    *
+    * This is used in the syntax: {{{game has smartBot called "Albert"}}}
+    */
   implicit val smartBot: EntitySyntacticSugar = SmartBotSyntacticSugar
+
+  /** Syntactic sugar to make the DSL more natural language-like
+    *
+    * This is used in the syntax: {{{game gives 3 cards to every player}}}
+    */
   implicit val to: ToSyntacticSugar = ToSyntacticSugar
+
+  /** Syntactic sugar to make the DSL more natural language-like
+    *
+    * This is not used
+    */
   implicit val from: FromSyntacticSugar = FromSyntacticSugar
+
+  /** Syntactic sugar to make the DSL more natural language-like
+    *
+    * This is used in the syntax: {{{game firstTurn starts from "Alice"}}}
+    */
   implicit val starts: StartsSyntacticSugar = StartsSyntacticSugar
+
+  /** Syntactic sugar to make the DSL more natural language-like
+    *
+    * This is used in the syntax: {{{game card points are:}}}
+    */
   implicit val points: PointsSyntacticSugar = PointsSyntacticSugar
+
+  /** Syntactic sugar to make the DSL more natural language-like
+    *
+    * This is used in the syntax: {{{game has team composedOf("Alice", "Bob")}}}
+    */
   implicit val team: TeamSyntacticSugar = TeamSyntacticSugar
+
+  /** Syntactic sugar to make the DSL more natural language-like
+    *
+    * This is used in the syntax: {{{game hand rules are:}}} And
+    * {{{game play rules are:}}} And {{{game win rules is:}}}
+    */
   implicit val rules: RulesSyntacticSugar = RulesSyntacticSugar
+
+  /** Syntactic sugar to make the DSL more natural language-like
+    *
+    * This is used in the syntax: {{{highest(suit) that takes is briscolaSuit}}}
+    * And {{{highest(rank) that takes follows first card suit}}}
+    */
   implicit val takes: TakesSyntacticSugar = TakesSyntacticSugar
+
+  /** Syntactic sugar to make the DSL more natural language-like
+    *
+    * This is used in the syntax: {{{highest(suit) that takes is briscolaSuit}}}
+    */
   implicit val suit: CardFilterSyntacticSugar = SuitSyntacticSugar
+
+  /** Syntactic sugar to make the DSL more natural language-like
+    *
+    * This is used in the syntax:
+    * {{{highest(rank) that takes follows first card suit}}}
+    */
   implicit val rank: CardFilterSyntacticSugar = RankSyntacticSugar
+
+  /** Syntactic sugar to make the DSL more natural language-like
+    *
+    * This is used in the syntax:
+    * {{{highest(rank) that takes follows first card suit}}}
+    */
   implicit val first: CardPositionSyntacticSugar = FirstCardSyntacticSugar
+
+  /** Syntactic sugar to make the DSL more natural language-like
+    *
+    * This is used in the syntax:
+    * {{{highest(rank) that takes follows last card suit}}}
+    */
   implicit val last: CardPositionSyntacticSugar = LastCardSyntacticSugar
 
   trait EntitySyntacticSugar extends SyntacticSugar

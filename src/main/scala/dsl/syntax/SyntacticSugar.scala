6e278ea3653383295380722ee2fb5d27abdf9a2c
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2025-07-09 11:27:57 +0200

    [docs] added scaladoc

--- src/main/scala/dsl/syntax/SyntacticSugar.scala
+++ src/main/scala/dsl/syntax/SyntacticSugar.scala
@@ -93,52 +93,86 @@ object SyntacticSugar:
     */
   implicit val last: CardPositionSyntacticSugar = LastCardSyntacticSugar
 
+  /** Trait to create either a player syntax sugar or a bot syntax sugar
+    */
   trait EntitySyntacticSugar extends SyntacticSugar
 
+  /** Trait to create a random bot syntax sugar
+    */
   trait RandomBotSyntacticSugar extends EntitySyntacticSugar
   private object RandomBotSyntacticSugar extends RandomBotSyntacticSugar
 
+  /** Trait to create a smart bot syntax sugar
+    */
   trait SmartBotSyntacticSugar extends EntitySyntacticSugar
   private object SmartBotSyntacticSugar extends SmartBotSyntacticSugar
 
+  /** Trait to create a player syntax sugar
+    */
   trait PlayerSyntacticSugar extends EntitySyntacticSugar
   private object PlayerSyntacticSugar extends PlayerSyntacticSugar
 
+  /** Trait to create a "to" syntax sugar
+    */
   trait ToSyntacticSugar extends SyntacticSugar
   private object ToSyntacticSugar extends ToSyntacticSugar
 
+  /** Trait to create a "from" syntax sugar
+    */
   trait FromSyntacticSugar extends SyntacticSugar
   private object FromSyntacticSugar extends FromSyntacticSugar
 
+  /** Trait to create a "starts" syntax sugar
+    */
   trait StartsSyntacticSugar extends SyntacticSugar
   private object StartsSyntacticSugar extends StartsSyntacticSugar
 
+  /** Trait to create a "points" syntax sugar
+    */
   trait PointsSyntacticSugar extends SyntacticSugar
   private object PointsSyntacticSugar extends PointsSyntacticSugar
 
+  /** Trait to create a "team" syntax sugar
+    */
   trait TeamSyntacticSugar extends SyntacticSugar
   private object TeamSyntacticSugar extends TeamSyntacticSugar
 
+  /** Trait to create a "rules" syntax sugar
+    */
   trait RulesSyntacticSugar extends SyntacticSugar
   private object RulesSyntacticSugar extends RulesSyntacticSugar
 
+  /** Trait to create a "takes" syntax sugar
+    */
   trait TakesSyntacticSugar extends SyntacticSugar
   private object TakesSyntacticSugar extends TakesSyntacticSugar
 
+  /** Trait to create a card filter syntax sugar
+    */
   trait CardFilterSyntacticSugar extends SyntacticSugar
   private object CardFilterSyntacticSugar extends CardFilterSyntacticSugar
 
+  /** Trait to create a "suit" syntax sugar
+    */
   trait SuitSyntacticSugar extends CardFilterSyntacticSugar
   private object SuitSyntacticSugar extends SuitSyntacticSugar
 
+  /** Trait to create a "rank" syntax sugar
+    */
   trait RankSyntacticSugar extends CardFilterSyntacticSugar
   private object RankSyntacticSugar extends RankSyntacticSugar
 
+  /** Trait to create a card position syntax sugar
+    */
   sealed trait CardPositionSyntacticSugar extends SyntacticSugar
   private object CardPositionSyntacticSugar extends CardPositionSyntacticSugar
 
+  /** Trait to create a "first" syntax sugar
+    */
   trait FirstCardSyntacticSugar extends CardPositionSyntacticSugar
   private object FirstCardSyntacticSugar extends FirstCardSyntacticSugar
 
+  /** Trait to create a "last" syntax sugar
+    */
   trait LastCardSyntacticSugar extends CardPositionSyntacticSugar
   private object LastCardSyntacticSugar extends LastCardSyntacticSugar

b8fc2dcfa19ebf461019cafddfdb39a4d477b671
Marco Pesic <marco.pesic@studio.unibo.it>
2025-07-11 15:51:16 +0200

    [docs] Added scala docs

--- src/main/scala/engine/view/PlayerViewManager.scala
+++ src/main/scala/engine/view/PlayerViewManager.scala
@@ -7,6 +7,15 @@ import engine.view.monads.States.State
 trait PlayerViewManager:
   var players: List[String] = List.empty
 
+  /** adds a Player to the game view.
+    *
+    * @param name
+    *   the name of the player
+    * @param numberOfPlayers
+    *   the total number of players in the game
+    * @return
+    *   a State that represents the action of adding a player to the game view
+    */
   def addPlayer(name: String, numberOfPlayers: Int): State[Frame, Unit] =
     players = players :+ name
     import WindowStateImpl.*
@@ -16,6 +25,15 @@ trait PlayerViewManager:
       _ <- setGridLayout(name, playerInfo._3)
     yield ()
 
+  /** * Retrieves the player position configuration based on the number of
+    * players.
+    *
+    * @param numberOfPlayers
+    *   the total number of players in the game
+    * @return
+    *   a tuple containing the coordinates for the player panel, the dimensions
+    *   of the panel, and the grid layout orientation for the player panel.
+    */
   private def getPlayerInfo(
       numberOfPlayers: Int
   ): ((Int, Int), (Int, Int), GridLayoutOrientation) =
